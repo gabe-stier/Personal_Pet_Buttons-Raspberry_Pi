e08fc8cd10210123cdebc943f9fae22efb27e17f
Gabe Stier <37159135+gabe-stier@users.noreply.github.com>
2022-04-08 16:49:29 -0500

    Force the sound src dir

--- app/managers/sound.py
+++ app/managers/sound.py
@@ -1,11 +1,22 @@
 from playsound import playsound
+from os import path
+from sys import modules
+
 
 class SoundManager:
+
+    dir = path.dirname(__file__)
+
     def __init__(self, sound_file):
-        self.sound_file = sound_file
+        if "__main__" in modules and hasattr(modules["__main__"],"__file__"):
+            self.dir = path.join(path.dirname(modules["__main__"].__file__),"sounds")
+        self.sound_file = path.join(self.dir, sound_file)
 
     def play(self):
-        playsound(self.sound_file)  
+        try:
+            playsound(self.sound_file)  
+        except:
+            pass
 
     def change_sound(self, sound_file):
-        self.sound_file = sound_file
+        self.sound_file = path.join(self.dir, sound_file)
