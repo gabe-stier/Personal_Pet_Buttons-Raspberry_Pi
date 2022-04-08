768348b86a5d7c65292660bef7f0592494dd75bb
Gabe Stier <37159135+gabe-stier@users.noreply.github.com>
2022-04-08 16:49:41 -0500

    File Cleanup

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,61 @@
 from app.common import config, messages
 from serial import Serial
+from typing import Tuple, List, Dict, Union
+from socket import socket, AF_INET, SOCK_STREAM
 
 from app.managers.http import APIManager
+from app.managers.sound import SoundManager
+from datetime import datetime
 
-def main():
+
+ObjectHint = Tuple[List[str], Dict[str, Union[str, SoundManager]]]
+restAPIActive = False
+
+
+def main() -> None:
     serial = Serial(config.com_port, config.rate)
-    messages_list = messages.messages
     api: APIManager = APIManager(config.ip, config.api_port, config.exponent, config.salt)
+    test_port(config.ip, config.api_port)
     while True:
         line = serial.readline().decode()
-        message_strings = [msg[0] for msg in messages_list]
+        message_strings, msgs = init_objects(messages.messages)
         if line in message_strings:
+            task(line, msgs[line]["uri"], msgs[line]['sound'], api)
             pass
 
-    pass
 
-def task(msg:str, uri:str, sound:str, api:APIManager):
-    pass
+def test_port(ip, port) -> None:
+    global restAPIActive
+    try:
+        s = socket(AF_INET, SOCK_STREAM)
+        s.connect(ip, port)
+        s.shutdown(2)
+        restAPIActive = True
+    except:
+        restAPIActive = False
+
+
+def task(msg: str, uri: str, sound: SoundManager, api: APIManager) -> bool:
+    global restAPIActive
+    sound.play()
+    message = {
+        "message": message,
+        "time": datetime.now()
+    }
+    if restAPIActive:
+        if api.send_request(None, "post", uri):
+            if api.send_request(None, "error", uri):
+                return None
+
+
+def init_objects(messages) -> ObjectHint:
+    msgs = {}
+    lines = []
+    for message in messages:
+        msgs[message[0]] = {"uri": message[1], "sound": SoundManager(message[2])}
+        lines.append(message[0])
+
+    return lines, msgs
 
 
 if __name__ == "__main__":
--- app/managers/http.py
+++ app/managers/http.py
@@ -3,21 +3,21 @@ from requests import get, post, put
 from app.managers.encryption import EncryptionManager
 from json import dumps
 
+
 class APIManager:
     fqdn = ''
     port = ''
     password = ''
-    methods = Literal['get','post','put']
+    methods = Literal['get', 'post', 'put']
+
     def __init__(self, ip, port, key_signature, salt) -> None:
         self.ip = ip
         self.port = port
         self.password = key_signature
         self.salt = salt
         self.ip = f"{self.ip}:{self.port}"
-        
 
-
-    def send_request(self, msg:dict, req_type:methods, uri:str):
+    def send_request(self, msg: dict, req_type: methods, uri: str):
         if req_type == "get":
             return True
         elif req_type == 'post':
